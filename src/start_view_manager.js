e165c0cbb7bcbd92000c9907293e6b11310158f6
cklapperich3 <cklapperich3@gatech.edu>
2025-02-02 10:15:50 -0600

    reorg

--- src/start_view_manager.js
+++
@@ -1,7 +0,0 @@
-// Initialize the view manager
-const viewManager = new ViewManager();
-
-// Start the game
-document.addEventListener('DOMContentLoaded', () => {
-    viewManager.show('menu');
-});
